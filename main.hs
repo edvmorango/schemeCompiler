4c17b9671dd6204f1873d774bd1a55e95be56e42
José Eduardo Vieira Morango <jmorango@br.imshealth.com>
2017-10-06 16:29:31 +0000

    Char, String,

--- main.hs
+++ main.hs
@@ -32,7 +32,9 @@ readExpr input = case parse parseExpr "lisp" input of
 parseExpr :: Parser LispVal
 parseExpr = parseAtom
         <|> parseNumber
-        <|> parseBool
+        <|> try parseBool
+        <|> try parseString
+        <|> try parseChar
         
 parseAtom :: Parser LispVal
 parseAtom = do
@@ -40,6 +42,33 @@ parseAtom = do
         rest <- many (letter <|> digit <|> symbol)
         let atom = first:rest
         return $ Atom atom
+
+parseString :: Parser LispVal
+parseString = do
+        char '"'
+        string <- many (noneOf ['\\', '"'])
+        char '"'
+        return $ String string
+
+escapedChars :: Parser Char
+escapedChars = do
+        char '\\'
+        c <- oneOf['\\', 'n', 'r', 't']
+        return $ case c of
+                '\\' -> c
+                '"' -> c
+                'n' -> '\n'
+                'r' -> '\r'
+                't' -> '\t'
+
+parseChar :: Parser LispVal
+parseChar = do
+        string "#\\"
+        c <- many1 letter
+        return $ case c of
+                "space" -> Char ' '
+                "newline" -> Char '\n'
+                [x] -> Char x
 
 parseNumber :: Parser LispVal
 parseNumber = (many1 digit) >>= (\n -> return ((Number . read) n))
